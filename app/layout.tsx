364103759c9f0662bdc32651bdaa48165d60fb56
matusilva <viniciusmatheus.dev@gmail.com>
2025-08-11 22:14:13 -0300

    criar componente button e reutilizar com merges

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
+import { Red_Hat_Display } from "next/font/google";
 
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -8,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`antialiased`}
-      >
+      <body className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}>
         {children}
       </body>
     </html>
